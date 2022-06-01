41f051832655437c6a8480dc6d816b451e9e5bbf
DianaBraun <braundiana8@gmail.com>
2022-06-01 15:55:10 +0300

    fixed basepage class

--- src/test/java/BuyProductTest.java
+++ src/test/java/BuyProductTest.java
@@ -11,6 +11,7 @@ public class BuyProductTest extends BaseTest{
         PDPage pdPage = new PDPage();
         open("/p/patagonia-arbor-roll-top-backpack-OEQPG03X000.html");
         pdPage.acceptCookie();
+        pdPage.closeCountryLayer();
         pdPage.addToBasket();
         pdPage.checkMiniBasket().shouldHave(Condition.text("1"));
     }
